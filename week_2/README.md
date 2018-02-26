57957ecbc90f0a9bd1937c1d5fd1a8bf4a4ed47e
Mario Geiger <geiger.mario@gmail.com>
2018-02-26 18:09:39 +0100

    week 2 add exercise

--- week_2/README.md
+++ week_2/README.md
@@ -13,3 +13,4 @@ Create a python library called `spam` such that `library.py` executes correctly
 - Complete the functions in `alg.py`
 - Observe the central limit theorem affirmation with histograms, `clt.py` contains an example how to plot an histogram
 - Implement the gradient descent https://en.wikipedia.org/wiki/Gradient_descent in `grad.py`
+- Bonus: plot the Mandelbrot set with numpy and matplotlib
--- week_2/decorators.py
+++ week_2/decorators.py
@@ -7,3 +7,35 @@ def foo(x, y):
 
 print(foo("Hello ", "world"))
 # Should print "foo('Hello ', 'world') = 'Hello world' on one line then print "Hello world" on the second line
+
+
+
+"""
+Implement a decorator called recursive_iterator that convert an function into an iterator
+
+You can make a decorator that takes something in argument by making a function that returns a decorator!
+
+Given a function F(z, *args) we want an iterator that generate z0, F(z0, *args), F(F(z0, *args), *args), ...
+"""
+
+@recursive_iterator(10)
+def inc(x):
+    return x + 1
+
+i = inc()
+assert next(i) == 10
+assert next(i) == 11
+assert next(i) == 12
+assert next(i) == 13
+
+
+@recursive_iterator(0)
+def mandlebrot(z, c):
+    return z ** 2 + c
+
+
+c = -0.1011 + 0.9563j
+i = mandlebrot(c)
+assert next(i) == 0
+assert next(i) == c
+assert next(i) == c ** 2 + c
