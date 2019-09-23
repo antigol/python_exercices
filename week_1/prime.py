7dafd10926b8f2199ae3bb91cdcef3fc84d488a0
Mario Geiger <geiger.mario@gmail.com>
2019-09-23 13:53:55 +0200

    prime

---
+++ week_1/prime.py
@@ -0,0 +1,15 @@
+# put some code here
+
+
+# such that the following code works as expected
+
+it = primes()
+
+assert next(it) == 1
+assert next(it) == 2
+assert next(it) == 3
+assert next(it) == 5
+assert next(it) == 7
+# all the prime numbers...
+
+# try to make is with the smallest possible code
