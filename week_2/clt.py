7bb2778196b48899698b8f6bd255b53da8045d52
Mario Geiger <geiger.mario@gmail.com>
2019-09-30 15:49:39 +0200

    move stuff

--- week_2/clt.py
+++ week_2/clt.py
@@ -1,10 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# In a jupyter-notebook you can add the line "%matplotlib inline"
 
 x = np.random.randn(1000000)
 plt.hist(x, bins=100)
-plt.show()  # and remove this line (in jupyter-notebook)
 
 
 # Show the consequences of the central limit theorem using numpy
